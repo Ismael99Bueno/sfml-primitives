16207df133bcc4e077401e53c5d647df980b8530
Ismael Bueno <ismaelwno@gmail.com>
2023-02-19 19:12:19 +0100

    Modified flat line strip to accomodate the predictor needs

--- include/flat_line_strip.hpp
+++ include/flat_line_strip.hpp
@@ -9,11 +9,21 @@ namespace prm
     class flat_line_strip : public sf::Drawable
     {
     public:
+        flat_line_strip(const sf::Color &color = sf::Color::White);
         flat_line_strip(const std::vector<alg::vec2> &points,
-                        const sf::Color color = sf::Color::White);
+                        const sf::Color &color = sf::Color::White);
+
+        void append(const alg::vec2 &point);
+        void clear();
+
+        const sf::Color &color() const;
+        const sf::VertexArray &vertices() const;
+
+        void color(const sf::Color &color);
 
     private:
         sf::VertexArray m_vertices;
+        sf::Color m_color;
 
         void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
     };
--- src/flat_line_strip.cpp
+++ src/flat_line_strip.cpp
@@ -2,8 +2,12 @@
 
 namespace prm
 {
+    flat_line_strip::flat_line_strip(const sf::Color &color) : m_vertices(sf::LineStrip),
+                                                               m_color(color) {}
+
     flat_line_strip::flat_line_strip(const std::vector<alg::vec2> &points,
-                                     const sf::Color color) : m_vertices(sf::LineStrip, points.size())
+                                     const sf::Color &color) : m_vertices(sf::LineStrip, points.size()),
+                                                               m_color(color)
     {
         for (std::size_t i = 0; i < points.size(); i++)
         {
@@ -12,8 +16,20 @@ namespace prm
         }
     }
 
+    void flat_line_strip::append(const alg::vec2 &point)
+    {
+        m_vertices.append({point, m_color});
+    }
+
+    void flat_line_strip::clear() { m_vertices.clear(); }
+
     void flat_line_strip::draw(sf::RenderTarget &target, sf::RenderStates states) const
     {
         target.draw(m_vertices, states);
     }
+
+    const sf::Color &flat_line_strip::color() const { return m_color; }
+    const sf::VertexArray &flat_line_strip::vertices() const { return m_vertices; }
+
+    void flat_line_strip::color(const sf::Color &color) { m_color = color; }
 }
