ebf67a5579615c67fb50515fd0f292d49bacdcf0
Ismael Bueno <ismaelwno@gmail.com>
2023-03-27 16:15:06 +0100

    Now bin/build folders are with respect the actual root folder

--- premake5.lua
+++ premake5.lua
@@ -5,8 +5,8 @@ project "sfml-primitives"
    cppdialect "C++20"
    staticruntime "on"
 
-   targetdir("../bin/" .. outputdir)
-   objdir("../build/" .. outputdir .. "/%{prj.name}")
+   targetdir("bin/" .. outputdir)
+   objdir("build/" .. outputdir .. "/%{prj.name}")
 
    files {"src/**.cpp", "include/**.hpp"}
    includedirs "../**/include"
