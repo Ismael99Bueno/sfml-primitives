6d5db5cba0f1bbcbd6389a4aeaa48abf2e904140
Ismael Bueno <ismaelwno@gmail.com>
2023-04-06 15:30:49 +0100

    Added epilog to argparser

--- scripts/generate_build_files.py
+++ scripts/generate_build_files.py
@@ -4,7 +4,10 @@ from utils import build_sfml, clean_sfml
 
 
 def main() -> None:
-    parser = ArgumentParser(description="Generate the build files for SFML.")
+    parser = ArgumentParser(
+        description="Generate the build files for SFML.",
+        epilog="Once the SFML build files have been generated, the project must have its own build files generated with premake, and compiled according to the chosen premake5 action.",
+    )
     parser.add_argument(
         "src",
         metavar="root-relpath",
