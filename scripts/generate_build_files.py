def67b9377d8764c9022338383ca1a9a88bcf2ff
Ismael Bueno <ismaelwno@gmail.com>
2023-04-09 23:33:53 +0100

    Renamed premake action to generator which is more standard

--- scripts/generate_build_files.py
+++ scripts/generate_build_files.py
@@ -5,7 +5,7 @@ from utils import build_sfml, clean_sfml, ROOT_PATH
 def main() -> None:
     parser = ArgumentParser(
         description="Generate the build files for SFML.",
-        epilog="Once the SFML build files have been generated, the project must have its own build files generated with premake, and compiled according to the chosen premake5 action.",
+        epilog="Once the SFML build files have been generated, the project must have its own build files generated with premake, and compiled according to the chosen generator.",
     )
     parser.add_argument(
         "--clean",
