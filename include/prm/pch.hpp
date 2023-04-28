4415ec2faf4551e77707f5ea627e416ab307a411
Ismael Bueno <ismaelwno@gmail.com>
2023-04-29 01:14:52 +0200

    Added pch files

---
+++ include/prm/pch.hpp
@@ -0,0 +1,10 @@
+#ifndef PRM_PCH_HPP
+#define PRM_PCH_HPP
+
+#include <SFML/Graphics.hpp>
+#include <glm/vec2.hpp>
+#include <cmath>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/geometric.hpp>
+
+#endif
---
+++ src/pch.cpp
@@ -0,0 +1,1 @@
+#include "prm/pch.hpp"
